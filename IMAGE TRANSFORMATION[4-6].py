949a77f43ae5bf0606275f51309941514afa5cd2
panduru somu <148988619+Pandurusomu@users.noreply.github.com>
2025-09-20 09:13:42 +0530

    Update IMAGE TRANSFORMATION[4-6].py

--- IMAGE TRANSFORMATION[4-6].py
+++ IMAGE TRANSFORMATION[4-6].py
@@ -1,37 +1,71 @@
+# Name : Panduru Somu
+# Reg No : 212223240111
 
-##iv)Image Reflection
-import numpy as np
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
-input_image=cv2.imread("got.jpg") 
-input_image=cv2.cvtColor(input_image,) 
-plt.axis("off") 
-plt.imshow(input_image)
+from google.colab import files
+
+uploaded = files.upload()
+
+image_path = list(uploaded.keys())[0]
+image = cv2.imread(image_path)
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+rows, cols, _ = image.shape
+
+M_translate = np.float32([[1, 0, 50], [0, 1, 100]])
+translated_image = cv2.warpAffine(image_rgb, M_translate, (cols, rows))
+
+scaled_image = cv2.resize(image_rgb, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_LINEAR)
+
+M_shear = np.float32([[1, 0.5, 0], [0.5, 1, 0]])
+sheared_image = cv2.warpAffine(image_rgb, M_shear, (int(cols * 1.5), int(rows * 1.5)))
+
+reflected_image = cv2.flip(image_rgb, 1)
+
+M_rotate = cv2.getRotationMatrix2D((cols / 2, rows / 2), 45, 1)
+rotated_image = cv2.warpAffine(image_rgb, M_rotate, (cols, rows))
+
+cropped_image = image_rgb[50:300, 100:400]
+
+plt.figure(figsize=(15, 10))
+plt.subplot(2, 3, 1)
+plt.imshow(image_rgb)
+plt.title("Original Image")
+plt.axis('off')
+
+plt.subplot(2, 3, 2)
+plt.imshow(translated_image)
+plt.title("Translated Image")
+plt.axis('off')
+
+plt.subplot(2, 3, 3)
+plt.imshow(scaled_image)
+plt.title("Scaled Image")
+plt.axis('off')
+
+plt.subplot(2, 3, 4)
+plt.imshow(sheared_image)
+plt.title("Sheared Image")
+plt.axis('off')
+
+plt.subplot(2, 3, 5)
+plt.imshow(reflected_image)
+plt.title("Reflected Image")
+plt.axis('off')
+
+plt.subplot(2, 3, 6)
+plt.imshow(rotated_image)
+plt.title("Rotated Image")
+plt.axis('off')
+
+plt.tight_layout()
 plt.show()
-rows, cols, dim = 
-M_x=np.float
-reflected_img_xaxis=cv2.warpPerspective(input_image,M_x,(cols,rows))
-reflected_img_yaxis=cv2.warpPerspective(input_image,M_y,(cols,rows))
-plt.imshow(reflected_)
-plt.show()
 
-##v)Image Rotation:
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-input_image=cv2.imread("got.jpg") 
-input_image=cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
-angle=np.radians(45)
-M=np.float32([[np.cos(angle),-(np.sin(angle)),0],
-               [np.
-rotated_img=
-
-##vi)Image Cropping:
-
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-input_image=cv2.imread("got.jpg") 
-input_image=cv2.cvtColor(input_image,
+plt.figure(figsize=(5, 5))
+plt.imshow(cropped_image)
+plt.title("Cropped Image")
+plt.axis('off')
 plt.show()
 
